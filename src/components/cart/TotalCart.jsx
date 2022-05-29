4e1e0afd10251062836856c8e47455d30042f6be
Arnold-Cruz <enriquecruzlife@gmail.com>
2022-05-28 23:56:15 -0600

    mejoras de interface

--- src/components/cart/TotalCart.jsx
+++ src/components/cart/TotalCart.jsx
@@ -14,48 +14,44 @@ function TotalCart() {
     setOpencart(false);
   };
   return (
-    <>
-      <div className={`Cart_Wrapper ${Opencart ? 'active' : ''}`}>
-        <div className="Cart_Content">
-          <div className="Cart_header">
-            <h3>Lista de Compras</h3>
-            <div className="down_arrow" onClick={handleOpenToCart}>
-              <i className="bx bxs-down-arrow"></i>
-            </div>
-          </div>
-          <div className="Cart_Pedido-Content">
-            {ITEM_TO_CART.map((item) => (
-              <div key={item.id} className="Cart_Pedido">
-                <img key={item.id} src={item.img} alt={item.largetitle} />
-                <div className="Cart_Pedido_title">
-                  <p>{item.largetitle}</p>
-                  <p>Precio:</p>
-                  <p>Unidades:</p>
-                  <p>Subtotal:</p>
-                </div>
-                <div className="Cart_Pedido_price">
-                  <p>{item.price}</p>
-                  <p>{item.qty}</p>
-                  <p>{getSubtotal(item)}</p>
-                </div>
-                <button onClick={() => handleDeleteItem(item)}>
-                  <img src={Papelera} alt="Eliminar" />
-                </button>
-              </div>
-            ))}
+    <div className={`Cart_Wrapper ${Opencart ? 'active' : ''}`}>
+      <div className="Cart_Content">
+        <div className="Cart_header">
+          <h3>Lista de Compras</h3>
+          <div className="down_arrow" onClick={handleOpenToCart}>
+            <i className="bx bxs-down-arrow"></i>
           </div>
         </div>
-
-        <div className={`Cart_total  ${Opencart ? 'active' : ''}`}>
-          <div>
-            <h3>Total:</h3>
-            <p>{getTotal()}</p>
-          </div>
-
-          <a onClick={() => orderList()}>Generar Pedido</a>
+        <div className="Cart_Pedido-Content">
+          {ITEM_TO_CART.map((item) => (
+            <div key={item.id} className="Cart_Pedido">
+              <img key={item.id} src={item.img} alt={item.largetitle} />
+              <div className="Cart_Pedido_title">
+                <p>{item.largetitle}</p>
+                <p>Precio:</p>
+                <p>Unidades:</p>
+                <p>Subtotal:</p>
+              </div>
+              <div className="Cart_Pedido_price">
+                <p>{item.price}</p>
+                <p>{item.qty}</p>
+                <p>{getSubtotal(item)}</p>
+              </div>
+              <button onClick={() => handleDeleteItem(item)}>
+                <img src={Papelera} alt="Eliminar" />
+              </button>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+      <div className={`Cart_total  ${Opencart ? 'active' : ''}`}>
+        <div>
+          <h3>Total:</h3>
+          <p>Lps.{getTotal()}</p>
+        </div>
+        <a onClick={() => orderList()}>Generar Pedido</a>
+      </div>
+    </div>
   );
 }
 export default TotalCart;
